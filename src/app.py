49cabe8a6733086efcd34e75c5f1ffc3b5d7a8f1
OliDeane <oliver.deane@gmail.com>
2023-08-30 15:06:55 +0100

    Changed code to test the document loading aspect

--- src/app.py
+++ src/app.py
@@ -13,61 +13,66 @@ import glob
 app = Dash(__name__)
 server = app.server
 
-df = px.data.gapminder()
+# df = px.data.gapminder()
 
-range_slider = dcc.RangeSlider(
-    value=[1987, 2007],
-    step=5,
-    marks={i: str(i) for i in range(1952, 2012, 5)},
-)
+# range_slider = dcc.RangeSlider(
+#     value=[1987, 2007],
+#     step=5,
+#     marks={i: str(i) for i in range(1952, 2012, 5)},
+# )
 
-dtable = dash_table.DataTable(
-    columns=[{"name": i, "id": i} for i in sorted(df.columns)],
-    sort_action="native",
-    page_size=10,
-    style_table={"overflowX": "auto"},
-)
+# dtable = dash_table.DataTable(
+#     columns=[{"name": i, "id": i} for i in sorted(df.columns)],
+#     sort_action="native",
+#     page_size=10,
+#     style_table={"overflowX": "auto"},
+# )
 
-download_button = html.Button("Download Filtered CSV", style={"marginTop": 20})
-download_component = dcc.Download()
+download_button = html.Button(id="download_button",children=["Download Filtered CSV"], style={"marginTop": 20})
+# download_component = dcc.Download()
 
 app.layout = html.Div(
     [
         html.H2("RAIMEE Data Download PySwipl", style={"marginBottom": 20}),
-        download_component,
-        range_slider,
+        # download_component,
+        # range_slider,
         download_button,
-        dtable,
+        html.Div(id="output", children = [])
+        # dtable,
     ]
 )
 
-@app.callback(
-    Output(dtable, "data"),
-    Input(range_slider, "value"),
-)
-def update_table(slider_value):
-    if not slider_value:
-        return dash.no_update
-    dff = df[df.year.between(slider_value[0], slider_value[1])]
-    return dff.to_dict("records")
+# @app.callback(
+#     Output(dtable, "data"),
+#     Input(range_slider, "value"),
+# )
+# def update_table(slider_value):
+#     if not slider_value:
+#         return dash.no_update
+#     dff = df[df.year.between(slider_value[0], slider_value[1])]
+#     return dff.to_dict("records")
 
 
 @app.callback(
-    Output(download_component, "data"),
-    Input(download_button, "n_clicks"),
-    State(dtable, "derived_virtual_data"),
-    prevent_initial_call=True,
+    Output("output", "children"),
+    Input("download_button", "n_clicks"),
+    # State(dtable, "derived_virtual_data"),
+    # prevent_initial_call=True,
 )
-def download_data(n_clicks, data):
-    dff = pd.DataFrame(data)
-    return dcc.send_data_frame(dff.to_csv, "filtered_csv.csv")
+def download_data(n_clicks):
+    # dff = pd.DataFrame(data)
+    if not n_clicks:
+        return no_update
+        # Load in a test file
+    with open('src/test.json', "w") as f:
+        json.dump({}, f)
+    return html.Div([html.P(str(n_clicks))])
+    # return dcc.send_data_frame(dff.to_csv, "filtered_csv.csv")
 
 
 if __name__ == "__main__":
 
-    # Load in a test file
-    with open('src/test.json', "w") as f:
-        json.dump({}, f)
+
 
     with PrologMQI() as mqi:
         with mqi.create_thread() as main_prolog_thread:
